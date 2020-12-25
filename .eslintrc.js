1e5b96b76d812025b2f8ab53b2deb3de4a0bcc69
Julien Papini <julien.papini@gmail.com>
2020-12-25 01:48:34 +0100

    develop: ESLint Simple Sort plugin installation

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,39 @@ module.exports = {
     'prettier/@typescript-eslint',
     'prettier/react',
   ],
+  plugins: ['simple-import-sort'],
   parserOptions: {
     project: './tsconfig.json',
     warnOnUnsupportedTypeScriptVersion: false,
   },
   rules: {
+    'sort-imports': 'off',
+    'import/order': 'off',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
+    'simple-import-sort/exports': 'error',
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [
+          // Node.js builtins
+          [`^(${require('module').builtinModules.join('|')})(/|$)`],
+          // Packages. `react` related packages come first.
+          ['^react', '^@?\\w'],
+          // Internal packages.
+          [`^@\/[^/]+(\/.*|$)`],
+          // Side effect imports.
+          ['^\\u0000'],
+          // Parent imports. Put `..` last.
+          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          // Style imports.
+          ['^.+\\.s?css$'],
+        ],
+      },
+    ],
     'react/jsx-props-no-spreading': 'off',
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
